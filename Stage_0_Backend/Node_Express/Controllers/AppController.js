43d37394bfe7f46d6b6ebe088a9f4e7e07f1fb43
SAVAGEAKALIA <akaliasaviour@gmail.com>
2025-01-30 10:18:01 +0100

    Updated github_url

--- Stage_0_Backend/Node_Express/Controllers/AppController.js
+++ Stage_0_Backend/Node_Express/Controllers/AppController.js
@@ -7,7 +7,7 @@ class AppController {
             const responseDetails = {
                 email: "akaliasaviour@gmail.com",
                 current_datetime: new Date(),
-                github_url: "https://github.com/SAVAGEAKALIA/HNG12/tree/main/Stage%200%20Backend"
+                github_url: "https://github.com/SAVAGEAKALIA/HNG12/tree/main/Stage_0_Backend/Node_Express"
             };
             // const now = new Date();
             // const year = now.getFullYear()
